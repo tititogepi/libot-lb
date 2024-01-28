bbff9c5fb8e8e2a0613f49c75c6a9b2e54ef67a6
IbaiBuR <ibaibur@chessnibble.com>
2024-01-28 19:58:58 +0100

    Modify flask app for website and removed unused html files

--- app.py
+++ app.py
@@ -19,29 +19,14 @@ TYPES = [
 ]
 
 @app.route('/')
-@app.route('/home')
-def welcome_page():
-    return send_file('html/Home.html')
-
 @app.route('/bot')
 def bot():
     return send_file('html/Bot.html')
-
-@app.route('/unrestricted')
-def unrestricted():
-    return send_file('html/Unrestricted.html')
 
 @app.route('/bot/<type_name>')
 def bot_type(type_name):
     if type_name in TYPES:
         return send_file(f'bot_leaderboard/{type_name}.html')
-    else:
-        return "Invalid type", 404
-
-@app.route('/unrestricted/<type_name>')
-def unrestricted_type(type_name):
-    if type_name in TYPES:
-        return send_file(f'unrestricted_bot_leaderboard/{type_name}.html')
     else:
         return "Invalid type", 404
 
