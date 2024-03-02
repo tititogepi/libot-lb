6d7a7730ca0f3caa50021c8bb7e6572bafc08191
IbaiBuR <ibaibur@chessnibble.com>
2024-03-02 12:44:45 +0100

    Update Leaderboard

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/bin/nohup gunicorn --config /home/ibaibur/libot-lb/gunicorn_config.py app:app >> /home/ibaibur/libot-lb/app.log &
+
+/home/ibaibur/.local/bin/gunicorn --config /home/ibaibur/libot-lb/gunicorn_config.py app:app
